44cd3f6e5211b8707d77ecee55c7c39defdacbab
Marco Azer <marco.azer.ca@gmail.com>
2015-09-10 00:45:59 -0400

    mysql connection not working

--- server.js
+++ server.js
@@ -1,18 +1,52 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var app = express();
+var http = require('http').Server(app);
+
+var connection = mysql.createConnection({
+	host:'localhost',
+	user: 'root',
+	password: 'root',
+	database: 'SundaySchool'
+});
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;
-
-var router = express.Router();
-
-router.get('/', function(req, res){
-	res.json({message: 'Hooray first API'});
+app.get('/', function(req, res){
+	var Data = {
+		'Data':'Hello Person'
+	};
+	res.json(Data);
 });
 
-app.use('/api', router);
-app.listen(port);
-console.log('Magic Happens on port ' + port);
+app.get('/person', function(req, res){
+	var Data = {
+		'Data':''
+	};
+
+	connection.query("SELECT * FROM person", function(err, rows, fields){
+		if(rows.length != 0){
+			Data["Data"] = rows;
+			res.json(Data);
+		}
+	});
+
+});
+
+http.listen(8080, function(){
+	console.log('Server is running');
+});
+
+//var port = process.env.PORT || 8080;
+//
+//var router = express.Router();
+//
+//router.get('/', function(req, res){
+//	res.json({message: 'Hooray first API'});
+//});
+//
+//app.use('/api', router);
+//app.listen(port);
+//console.log('Magic Happens on port ' + port);
