442c3161265cb84944d49d8662cddba23ca73cd0
unknown <mazer@MJLDEMB-7034DL9.rim.net>
2015-10-08 16:02:00 -0400

    Know how to use resources

--- angular/main.js
+++ angular/main.js
@@ -1,12 +1,23 @@
-var app = angular.module('main', []);
+var app = angular.module('main', ['ngResource']);
 
-app.controller('DBCtrl', ['$scope', '$http', function($scope, $http){
+app.factory('dataFac', function($resource){
+	var getData = $resource('http://localhost:8080/');
+	var data = getData.get();
+	return data;
+});
+
+app.controller('DBCtrl', ['$scope', '$http', 'dataFac', function($scope, $http, dataFac){
+	// $scope.getData = function(){
+	// 	$http.get('http://localhost:8080').then(function(res){
+	// 		console.log("Got Data")
+	// 		console.log(res.data)
+	// 	}, function(res){
+	// 		console.log("Error Getting data")
+	// 	})
+	// }
 	$scope.getData = function(){
-		$http.get('http://localhost:8080').then(function(res){
-			console.log("Got Data")
-			console.log(res.data)
-		}, function(res){
-			console.log("Error Getting data")
-		})
+		console.log("YOO");
+		console.log(dataFac);
 	}
+		
 }]);
