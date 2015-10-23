c71e2ab6562f3c9b99015c8ed08f0de02c695e07
unknown <mazer@MJLDEMB-7034DL9.rim.net>
2015-10-23 17:19:30 -0400

    Added all routes but couldn't test at work

--- angular/main.js
+++ angular/main.js
@@ -31,12 +31,31 @@ var app = angular.module('main', ['ngRoute']);
 app.config(function($routeProvider){
 	$routeProvider
 	.when('/', {
-		templateUrl: 'pages/home.html',
-		controller: ''
+		templateUrl: '../pages/home.html'
 	})
 	.when('/addStudent', {
-		templateUrl: 'pages/AddStudent.html'.
+		templateUrl: '../pages/NewStudent.html',
 		controller: 'AddStudentCtrl'
+	})
+	.when('/searchStudent',{
+		templateUrl: '../pages/SearchStudent.html',
+		controller: 'SearchStudentCtrl'
+	})
+	.when('/NewServant', {
+		templateUrl: '../pages/NewServant.html',
+		controller: 'SearchServantCtrl'
+	})
+	.when('/SearchServant', {
+		templateUrl: '../pages/SearchServant.html',
+		controller: 'SearchServantCtrl'
+	})
+	.when('/NewClass', {
+		templateUrl: '../pages/NewClass.html'
+		controller: 'AddClassCtrl'
+	})
+	.when('/SearchClass', {
+		templateUrl: '../pages/SearchClass.html',
+		controller: 'SearchClassCtrl'
 	})
 });
 
