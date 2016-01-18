e15004606d2d1fdbbbbd08e97aa019ae179f3fd5
Marco Azer <Marco-Azer@users.noreply.github.com>
2016-01-18 11:36:22 -0500

    Delete httpServer.js

--- httpServer.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var fs = require('fs');
-var app = express();
-app.get('/', function(req, res){
-	res.sendFile('index.html');
-});
-app.listen(8080, function(){
-	console.log("Server is running");
-});
